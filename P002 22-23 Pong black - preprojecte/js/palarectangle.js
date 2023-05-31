724e3d5e8b252fa519545bd6567f44e8359fe372
DavidPujol <dpujol@edu.tecnocampus.cat>
2023-05-31 18:35:16 +0200

    palarectangle ok
    moviment de la pala normal i automàtic

--- P002 22-23 Pong black - preprojecte/js/palarectangle.js
+++ P002 22-23 Pong black - preprojecte/js/palarectangle.js
@@ -16,12 +16,14 @@ class PalaRectangle extends Rectangle{
          * Tasca. Definir el moviment de la pala
          * en funció de la telca premuda
         **********************************/
+            this.mou(0,alcada);
         }
         if(key.UP.pressed){
        /********************************* 
          * Tasca. Definir el moviment de la pala
          * en funció de la telca premuda
         **********************************/
+            this.mou(0,-alcada);
         }
     }
     updateAuto(alcada){
@@ -30,6 +32,12 @@ class PalaRectangle extends Rectangle{
          * automàtica en moviment constant 
          * o amb variacions aleatories
         **********************************/
+        const direction = Math.random() < 0.5 ? 1 : -1; // Determinar la direcció aleatoriament
+
+        // Calcular el desplaçament aleatori en el eix Y
+        const movement = Math.floor(Math.random() * alcada) * direction;
+
+        this.mou(0, movement);
 
     }
 
